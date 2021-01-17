ed53f58b0f7114b394ec35976a34e58611e9b613
programando <jhonjamesmg@hotmail.com>
2021-01-17 16:58:14 -0500

    Conexión con la base de datos

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;1,400&display=swap'
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap',
       },
     ]
   },
