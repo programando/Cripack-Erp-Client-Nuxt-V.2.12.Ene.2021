ef50ff7458a2982dfa53ee9d43512e4f9e922106
programando <jhonjamesmg@hotmail.com>
2021-05-14 20:22:33 -0500

    Change route

--- models/Terceros.js
+++ models/Terceros.js
@@ -4,7 +4,7 @@ import Api  from "@/config/Axios";
 export default {
   async historialVentas(formData) {
     //console.log(formData);
-    return Api.get("/clientes/ots", { params:   formData   } ) ;
+    return Api.get("/clientes/historial/ots", { params:   formData   } ) ;
   },
 
 
