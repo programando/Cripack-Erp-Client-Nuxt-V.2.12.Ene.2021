8d933c7d7499956d7bc99e58b62ff9d6aa66a4cd
programando <jhonjamesmg@hotmail.com>
2021-05-21 14:39:18 -0500

    Cambio rutas

--- components/comunes/header.vue
+++ components/comunes/header.vue
@@ -7,10 +7,11 @@
         <h3 class="font-semibold"> {{ contactName }}</h3>   
       </div>
     </div>
+    
     <div class="flex mr-4 space-x-6">
        <NavLinks  text="Historial" to="/clientes/ots-historial" />
       
-       <div class="relative text-white">
+<!--        <div class="relative text-white">
          <button @click="menu = !menu" class="px-2 font-semibold">Solicitud OT</button>
          <div v-if="menu" class="absolute z-20 flex flex-col pb-2 text-sm border border-white rounded-md top-7 w-28 bg-azul">
            <div @click="menu = !menu" class="mt-2 hover:opacity-90">
@@ -23,7 +24,8 @@
              <nuxt-link  class="px-2 py-1 text-white " to="/clientes/ordenes-trabajo/troquel-plano">Troquel Plano</nuxt-link>
            </div>
          </div>
-       </div>
+       </div> -->
+
       <NavLinks  text="Estado OT's" to="/clientes/ots-estado" />
 
 
@@ -40,6 +42,7 @@
       /> -->
        <button @click="logout" class="font-semibold text-white"> Salir </button> 
     </div>
+
   </div>
 </template>
 
--- models/Terceros.js
+++ models/Terceros.js
@@ -4,11 +4,11 @@ import Api  from "@/config/Axios";
 export default {
   async historialVentas(formData) {
     //console.log(formData);
-    return Api.get("/clientes/historial/ots", { params:   formData   } ) ;
+    return Api.get("clientes/ots/historial", { params:   formData   } ) ;
   },
 
   async OTsEstadoProduccion(formData) {
-    return Api.get("/clientes/estado/ots", { params:   formData   }     ) ;
+    return Api.get("clientes/ots/estado", { params:   formData   }     ) ;
   },
 
 }
