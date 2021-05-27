0a67429ea5b77e8b77a486cf774265726e7eab2b
programando <jhonjamesmg@hotmail.com>
2021-05-26 20:38:14 -0500

    !menu solicitudes

--- components/comunes/header.vue
+++ components/comunes/header.vue
@@ -11,7 +11,7 @@
     <div class="flex mr-4 space-x-6">
        <NavLinks  text="Historial" to="/clientes/ots-historial" />
       
-       <div class="relative text-white">
+<!--        <div class="relative text-white">
          <button @click="menu = !menu" class="px-2 font-semibold">Solicitud OT</button>
          <div v-if="menu" class="absolute z-20 flex flex-col pb-2 text-sm border border-white rounded-md top-7 w-28 bg-azul">
            <div @click="menu = !menu" class="mt-2 hover:opacity-90">
@@ -24,7 +24,7 @@
              <nuxt-link  class="px-2 py-1 text-white " to="/clientes/ordenes-trabajo/troquel-plano">Troquel Plano</nuxt-link>
            </div>
          </div>
-       </div>
+       </div> -->
 
       <NavLinks  text="Estado OT's" to="/clientes/ots-estado" />
 
