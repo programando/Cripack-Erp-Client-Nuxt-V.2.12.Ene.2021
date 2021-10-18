18a22cd6ef12ff6b105dda3bd2839af69d2dd303
Luis Morales <luisjose.morales2@gmail.com>
2021-10-18 10:01:36 -0400

    modificando tabla bitacora, agregando filtros

--- pages/gestion/bitacora.vue
+++ pages/gestion/bitacora.vue
@@ -11,7 +11,7 @@
         <input
           type="text"
           class="w-64 px-4 py-2 border rounded focus:outline-none"
-          v-model="buscarUsuario"
+          v-model="buscarArchivo"
         />
       </form>
     </div>
@@ -27,9 +27,9 @@
           </thead>
           <tbody class="w-full">
             <tr v-for="usuario in usuarioFiltrado" :key="usuario.idarchivo" class="border">
-              <td class="w-4/12 px-4 py-1 text-sm">{{ usuario.nomarchivo }}</td>
-              <td class="w-7/12 py-1 text-sm">{{ usuario.observacion }}</td>
-              <td class="w-1/12 cursor-pointer"><img class="w-8 lg:ml-14 xl:ml-16" src="/images/pdf.svg" alt="" @click = "pdfView ( usuario.file_key)" > </td>
+              <td class="w-4/12 px-4 py-1 text-sm">{{ usuario.nomarchivo | Capitalize }}</td>
+              <td class="w-7/12 py-1 text-sm">{{ usuario.observacion | Capitalize}}</td>
+              <td class="w-1/12 cursor-pointer"><img class="w-8 h-5 lg:ml-14 xl:ml-16" src="/images/pdf.svg" alt="" @click = "pdfView ( usuario.file_key)" > </td>
             </tr>
           </tbody>
         </table>
@@ -45,16 +45,44 @@ export default {
   name: "Bitacora",
   data() {
     return {
-      usuarios: [
+      archivos: [
         {
-          idarchivo: 0,
-          nomarchivo: '',
-          observacion: '',
-          palabrasclave :'',
+          idarchivo: 3,
+          idcategoria: 1,
+          nomarchivo: "ajustes picos.pdf",
+          extension: "",
+          observacion: "ajustar calibracion de la dimensión de los picos en la Hugo",
+          palabrasclave: "HUGO AJUSTE CALIBRACION PICOS",
         },
+        {
+        idarchivo: 15,
+        idcategoria: 1,
+        nomarchivo: "calibracion origen.pdf",
+        extension: "",
+        observacion: "calibración origen dobladora hidraúlica",
+        palabrasclave: "PERDIDA CALIBRACION ORIGEN HIDRAULICA",
+        restringido: false,
+        ruta: "",
+        file_key: "5714wojx130921070509.PDF",
+        fcha_rgstro: "2021-09-01 15:51:53",
+        cnslta_web: 1
+      },
+      {
+        idarchivo: 32,
+        idcategoria: 2,
+        nomarchivo: "en acanaladora.pdf",
+        extension: "",
+        observacion: "instrucciones para el envio de acanalados a maquina acanaladora",
+        palabrasclave: "ACANALADORA ENVIO DATOS INFORMACION",
+        restringido: false,
+        ruta: "",
+        file_key: "5714xpsa130921070557.PDF",
+        fcha_rgstro: "2021-09-01 15:51:53",
+        cnslta_web: 1
+    },
          
       ],
-      buscarUsuario:'',
+      buscarArchivo:'',
     };
   },
   
@@ -65,9 +93,9 @@ export default {
   computed: {
     //  la busqueda se hace en el campo  "palabrasclave"
     usuarioFiltrado() {
-      return this.usuarios.filter(usuario => {
-        let nameUp = this.buscarUsuario.toUpperCase()
-        return usuario.nomarchivo.includes(nameUp)
+      return this.archivos.filter(usuario => {
+        let nameUp = this.buscarArchivo.toUpperCase()
+        return usuario.palabrasclave.includes(nameUp)
       })
     } 
   },
@@ -76,7 +104,7 @@ export default {
       getDocuments () {
           Documentacion.getDocuments ()
           .then ( response => {           
-              this.usuarios = response.data;
+              // this.archivos = response.data;
           })
       },
     pdfView ( pdfFile ) {
