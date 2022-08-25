efc97d13ade93e5516147612f46e405549ab0f67
Luis Morales <luisjose.morales2@gmail.com>
2022-08-25 10:25:46 -0400

    modificando estados checkbox

--- components/gestion/TableProductos.vue
+++ components/gestion/TableProductos.vue
@@ -18,7 +18,7 @@
             :key="producto.id_grupo_estlo"
           >
             <td class="w-10 px-2 py-2 text-xs border text-azul cursor-pointer">
-              <input class="cursor-pointer" type="checkbox" name="" id="" />
+              <input v-model="producto.check" class="cursor-pointer" type="checkbox" name="" id="" />
             </td>
             <td class="w-40 px-4 py-2 text-lowerxs text-left border">
               {{ producto.nom_group_estlo }}
@@ -31,8 +31,8 @@
       </table>
     </div>
     <div class="mt-2">
-      <input class="ml-2" type="checkbox" name="" id="" />
-      <label for="">Todos</label>
+      <input v-model="todos" class="ml-2" type="checkbox" name="" id="todos" />
+      <label for="todos">Todos</label>
     </div>
   </div>
 </template>
@@ -47,56 +47,82 @@ export default {
         {
           id_grupo_estlo: 1,
           nom_group_estlo: "grupo 1",
-          alias_grup_estlo: "producto 1"
+          alias_grup_estlo: "producto 1",
+          check: false
         },
         {
           id_grupo_estlo: 2,
           nom_group_estlo: "grupo 2",
-          alias_grup_estlo: "producto 2"
+          alias_grup_estlo: "producto 2",
+          check: false
         },
         {
           id_grupo_estlo: 3,
           nom_group_estlo: "grupo 3",
-          alias_grup_estlo: "producto 3"
+          alias_grup_estlo: "producto 3",
+          check: false
         },
         {
           id_grupo_estlo: 4,
           nom_group_estlo: "grupo 4",
-          alias_grup_estlo: "producto 4"
+          alias_grup_estlo: "producto 4",
+          check: false
         },
         {
           id_grupo_estlo: 5,
           nom_group_estlo: "grupo 5",
-          alias_grup_estlo: "producto 5"
+          alias_grup_estlo: "producto 5",
+          check: false
         },
         {
           id_grupo_estlo: 6,
           nom_group_estlo: "grupo 6",
-          alias_grup_estlo: "producto 6"
+          alias_grup_estlo: "producto 6",
+          check: false
         },
         {
           id_grupo_estlo: 7,
           nom_group_estlo: "grupo 7",
-          alias_grup_estlo: "producto 7"
+          alias_grup_estlo: "producto 7",
+          check: false
         },
         {
           id_grupo_estlo: 8,
           nom_group_estlo: "grupo 8",
-          alias_grup_estlo: "producto 8"
+          alias_grup_estlo: "producto 8",
+          check: false
         },
         {
-          id_grupo_estlo: 8,
-          nom_group_estlo: "grupo 8",
-          alias_grup_estlo: "producto 8"
+          id_grupo_estlo: 9,
+          nom_group_estlo: "grupo 9",
+          alias_grup_estlo: "producto 9",
+          check: false
         },
         {
-          id_grupo_estlo: 8,
-          nom_group_estlo: "grupo 8",
-          alias_grup_estlo: "producto 8"
+          id_grupo_estlo: 10,
+          nom_group_estlo: "grupo 10",
+          alias_grup_estlo: "producto 10",
+          check: false
         }
-      ]
+      ],
+      todos: false
     };
-  }
+  },
+
+  computed: {
+    setTodos() {
+      if (this.todos == true) {
+        this.productos.map(el => {
+          return el.check = true
+        })
+      } else {
+        this.productos.map(el => {
+          return el.check = false
+        })
+      }
+    }
+  },
+
 };
 </script>
 
